3391970036f698a7491e8182f5df6cce96db367f
MIkayel17071996 <mikayel.papakhchyan@gmail.com>
2025-09-14 21:29:42 +0400

    add vercel.json

--- CAM-NGO/vite.config.js
+++ CAM-NGO/vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: process.env.VITE_BASE_PATH || '/',
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  server: { port: 5173 },
+
+  // base : process.env.VITE_BASE_PATH || '/JM-TECH'
+
 })
